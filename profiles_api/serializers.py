1c09c22ca9f5eef4d35e16ddbba5a90dc23425fc
anuppathak <anup@keywordio.com>
2020-06-15 14:03:49 +0530

    Added POST PUT and DElete

---
+++ profiles_api/serializers.py
@@ -0,0 +1,5 @@
+from rest_framework import serializers
+
+class HelloSerializer(serializers.Serializer):
+    """docstring for HelloSerializer."""
+    name = serializers.CharField(max_length = 10)
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework import status
+from profiles_api import serializers
 
 class HelloApiView(APIView):
     """Test Api view"""
+    serializer_class =serializers.HelloSerializer
     def get(self, request,format=None):
         """Returns a list of APIView features"""
         an_apiview = ['Uses HTTP methods as function (get,post,patch,put,delete)',
@@ -12,3 +14,27 @@ class HelloApiView(APIView):
         'Is mapped manually to urls',]
 
         return Response({'message':'Hello','an_apiview':an_apiview})
+
+    def post(self,request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data['name']
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+    """3. put """
+    def put(self, request, pk = None):
+        """ Handle updating an object """
+        return Response({'method': 'PUT'})
+    """4. patch """
+    def patch(self, request, pk = None):
+        """ Handle a partial updating an object """
+        return Response({'method': 'PATCH'})
+    """5. delete """
+    def delete(self, request, pk = None):
+        """ Handle a partial updating an object """
+        return Response({'method': 'DELETE'})
